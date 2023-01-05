7ce5ef810a4671dca7e2368277fc16d7acc2bcda
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-05 11:15:22 -0300

    Criação da classe Noticia

---
+++ src/main/java/org/example/model/Noticia.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import java.util.Date;
+
+public class Noticia {
+    private long id;
+    private String titulo;
+    private String texto;
+    private Date data;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
